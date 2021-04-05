ab606588487b0eb7fae07c0663f4e7a334e3fbb8
RudikS <rudik005@gmail.com>
2021-04-05 21:07:33 +0400

    Add project files.

---
+++ LAB_MndMAPS__SymplexMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB_MndMAPS__SymplexMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
